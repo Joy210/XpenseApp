b3d1a92a11574783dacd76db37b040411e427334
joy210 <sohanoor404@gmail.com>
2023-01-03 00:25:17 +0600

    firebase-config

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCf3Yt9D20GVVHKDkGyQ6hRkqVXGSArltU",
+  authDomain: "xpense-app-bc1f4.firebaseapp.com",
+  projectId: "xpense-app-bc1f4",
+  storageBucket: "xpense-app-bc1f4.appspot.com",
+  messagingSenderId: "474451321845",
+  appId: "1:474451321845:web:0782a82f20ddd83cb576bc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
